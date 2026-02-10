1b2c14ca4766c86c4ed6117ca162f9db37526731
ndtribk <ndtribk@gmail.com>
2026-02-10 13:59:24 +0700

    binding_guard.cpp

---
+++ bindings/core/binding_guard.cpp
@@ -0,0 +1,13 @@
+#include "binding_guard.h"
+#include <iostream>
+
+#include <string> 
+using namespace std;
+
+namespace aic 
+{
+    static bool check()
+    {
+        
+    }
+}
---
+++ bindings/core/binding_guard.h
@@ -0,0 +1,13 @@
+#pragma once 
+#include <iostream> 
+
+using namespace std; 
+
+namespace aic 
+{
+    class BindingGuard 
+    {
+        public: 
+        static bool check(); 
+    }; 
+}
